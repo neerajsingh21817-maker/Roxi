387599725e0f8fc59bd1b699705f1024de5d444b
neerajsingh21817-maker <neerajsingh21817@gmail.com>
2025-08-07 11:39:59 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+API_ID = 23549525
+API_HASH = "0958efc563b9a6710afd7176b736af88"
+BOT_TOKEN = "8205507089:AAE8s4sZIlkH0BLvd9k6SsnSdAFj1WlQf-o"
+
+# Delivery channel ID (must be negative for private/public channels)
+DELIVERY_CHANNEL = -1002601317517
+
+# ShrinkMe API key
+SHRINKME_API = "a72166cda2fcb2ca33aaf7b1353ae8790097eacc"
