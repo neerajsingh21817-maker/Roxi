12e6a9c1b6c21e03992462ac191826cefeab3afa
neerajsingh21817-maker <neerajsingh21817@gmail.com>
2025-08-07 11:41:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-API_ID = 23549525
-API_HASH = "0958efc563b9a6710afd7176b736af88"
-BOT_TOKEN = "8205507089:AAE8s4sZIlkH0BLvd9k6SsnSdAFj1WlQf-o"
+import os
 
-# Delivery channel ID (must be negative for private/public channels)
-DELIVERY_CHANNEL = -1002601317517
-
-# ShrinkMe API key
-SHRINKME_API = "a72166cda2fcb2ca33aaf7b1353ae8790097eacc"
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SHRINKME_API = os.getenv("SHRINKME_API")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+OWNER_ID = int(os.getenv("OWNER_ID"))
